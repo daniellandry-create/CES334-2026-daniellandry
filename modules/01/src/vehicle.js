c45c5d6942d9323ee9b21e671d83feb876d7b4b6
Daniel Landry <daniel.landry@yale.edu>
2026-09-14 22:12:07 -0400

    update braitenberg simulation

--- modules/01/src/vehicle.js
+++ modules/01/src/vehicle.js
@@ -1,9 +1,40 @@
+ 
+const wirings = {
+                    //[Speeds up closer to light, turns toward light]
+    fear:           s => [1/s.dist, s.dot*s.omega], 
+    aggression:     s =>[1/s.dist, -s.dot*s.omega],
+    love:           s => [1- (1/s.dist), s.dot*s.omega],
+    explorer:       s => [1 - (1/s.dist), -s.dot*s.omega]
+}
 
 class vehicle {
-    constructor(love, explorer, aggressive, coward){
-        this.love = love;
-        this.explorer = explorer;
-        this.aggressive = aggressive;
-        this.coward = coward;
+    constructor(startX, startY, options){
+        
+        //ensures at least SOMETHING was input for options 
+        if (options == undefined){
+            options = {}; 
+        }
+
+        //initializes vehicle position
+        this.x = startX;
+        this.y = startY;
+        
+        //makes sure the option inputed is among the four defined options. If not, default to aggression
+        if (options.wiring !== undefined){
+            this.wiring = options.wiring;
+        } else{
+            this.wiring = wirings/aggression
+        }
+
+        for (let key in options){
+            if (key != wiring){
+                this[key] = options[key];
+            }
+        }
+
+    }
+
+    update(){
+        
     }
 }
